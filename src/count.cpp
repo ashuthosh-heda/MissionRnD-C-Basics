564daf14ea72cb8be04d518bb8906b9f860ff394
ashuthosh-heda <ashuthosh.heda@gmail.com>
2015-11-01 19:20:33 +0530

    Submit Commit

--- src/count.cpp
+++ src/count.cpp
@@ -12,5 +12,22 @@ ERROR CASES: Return -1 in error cases.
 NOTES:
 */
 int count(int num1, int num2) {
-	return 0;
+	if (num1 > num2 || num1 == num2){
+		return 0;
+	}
+	
+	if (num2 < 0 || num1 <= 0){
+		return -1;
+	}
+
+	int temp = num1;
+	int counter = 0;
+
+	while (temp <= num2){
+		temp += num1;
+		if (temp % num1 == 0){
+			counter++;
+		}
+	}
+	return counter;
 }
